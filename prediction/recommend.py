339cbd3fcdd096b6197c47e5ebca867849e3326d
Sophie <sophie@millionroads.com>
2022-05-26 20:00:35 +0200

    Return array of article ids

--- prediction/recommend.py
+++ prediction/recommend.py
@@ -4,4 +4,4 @@ def recommend(user_id: str):
     """
     Predict 5 articles for user_id. Return array of 5 article_id.
     """
-    return [f'{(user_id)}-{item}' for item in [10,20,30,40,50]]
+    return [34, 32, 893, 1, 424]

1872d2237dde36a1fbb4f59bd828f9b12fb8ddb3
Sophie <sophie@millionroads.com>
2022-06-27 21:03:31 +0200

    Visual layout

--- prediction/recommend.py
+++ prediction/recommend.py
@@ -14,18 +14,21 @@ def recommend(user_id: str, only_on_liked=False):
     Return array of article_ids recommended for user_id.
     """
     logging.warning(f'Getting clicks for user {user_id}...')
+
     try:
         user_data = dr.get_data_for_user(user_id)
     except ValueError:
         logging.error('User not found, returning most popular articles.')
         most_read = dr.get_most_read_articles_ids()
         return most_read
+
     logging.warning(f'Got clicks for user {user_id}.')
     logging.warning(f'Scoring articles for user {user_id}...')
     scored_articles = sc.compute_scores(user_data)
     if only_on_liked:
         scored_articles = scored_articles[scored_articles['score'] == 1]
     logging.warning(f'Scored articles for user {user_id}.')
+
     logging.warning(f'Recommending articles for user {user_id}...')
     articles_ids = scored_articles['click_article_id'].astype(str).values
     recommendations = rec.recommend_from(articles_ids)

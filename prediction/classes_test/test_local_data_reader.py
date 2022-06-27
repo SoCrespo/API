650dd6cc2384a663275ad01fb2ea80a1738559ae
Sophie <sophie@millionroads.com>
2022-06-27 21:51:25 +0200

    Add test, edit fixture name in tests

--- prediction/classes_test/test_local_data_reader.py
+++ prediction/classes_test/test_local_data_reader.py
@@ -10,21 +10,25 @@ from prediction.classes.local_data_reader import LocalDataReader
 import pandas as pd
 
 @pytest.fixture
-def result():
+def user_data():
     return LocalDataReader().get_data_for_user('0')    
 
 @pytest.fixture
 def most_read():
-    return LocalDataReader().get_most_read_articles_ids()    
+    return LocalDataReader().get_most_read_articles_ids() 
 
-def test_user_data_loader_is_df(result):
-    assert isinstance(result, pd.DataFrame)
+@pytest.fixture
+def embeddings():
+    return LocalDataReader().get_embeddings()
 
-def test_user_data_not_empty(result):
-    assert not result.empty
+def test_user_data_loader_is_df(user_data):
+    assert isinstance(user_data, pd.DataFrame)
 
-def test_user_data_loader_has_expected_cols(result):
-    assert [col in result.columns for col in 
+def test_user_data_not_empty(user_data):
+    assert not user_data.empty
+
+def test_user_data_loader_has_expected_cols(user_data):
+    assert [col in user_data.columns for col in 
     ['user_id', 
     'click_article_id', 
     'session_id',
@@ -37,3 +41,6 @@ def test_most_read_return_list(most_read):
 
 def test_most_read_is_not_empty(most_read):
     assert len(most_read) > 0
+
+def test_get_embeddings(embeddings):
+    assert embeddings.shape[1] ==  251
